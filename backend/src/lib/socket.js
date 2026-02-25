c6d4aa9bdfd01a809b65d1dd682d4ec07a5bd712
Mahipat Makwana <86786837+MAFIA003@users.noreply.github.com>
2026-02-26 01:35:04 +0530

    Project completed

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,44 @@
+import {Server} from 'socket.io';
+import http from 'http';
+import express from 'express';
+
+
+const app = express();
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173']
+    }
+});
+
+export function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+};
+
+// To store online users
+const userSocketMap = {};
+
+
+io.on("connection", (socket) => {
+    console.log("a user connected: ", socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+    }
+
+    // io.emit() is used to send events to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected: ", socket.id);
+        // Remove the user from the online users list
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
+
+export {io, server, app};
+
